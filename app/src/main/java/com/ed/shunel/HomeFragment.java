8466128056cf59bda8f16911faf7839067a8c748
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-06-30 00:46:58 +0800

    no message

--- app/src/main/java/com/ed/shunel/HomeFragment.java
+++ app/src/main/java/com/ed/shunel/HomeFragment.java
@@ -1,12 +1,14 @@
 package com.ed.shunel;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 
 /**
@@ -14,10 +16,17 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
+    private final static String TAG="HomeFragment";
+    private FragmentActivity activity;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity=getActivity();
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,4 +34,14 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
+    }
+
 }
