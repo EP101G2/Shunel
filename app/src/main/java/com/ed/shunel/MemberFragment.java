9ded2e54b9726993a687dffc21800d22aa800461
Yuan Che <edwarechang7878@hotmail.com>
2020-07-01 19:51:28 +0800

    建立會員中心跳到頁面程式碼

--- app/src/main/java/com/ed/shunel/MemberFragment.java
+++ app/src/main/java/com/ed/shunel/MemberFragment.java
@@ -1,28 +1,47 @@
 package com.ed.shunel;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class MemberFragment extends Fragment {
 
-    public MemberFragment() {
-        // Required empty public constructor
+  private CardView cvSetting;
+private Activity activity;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity=getActivity();
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_member, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cvSetting=view.findViewById(R.id.cvSetting);
+        cvSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_memberFragment_to_settingFragment2);
+            }
+        });
     }
 }
