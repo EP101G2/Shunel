35c549de4f7fd75c9e8991ab5951681ba7592a0b
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-07-03 01:19:29 +0800

    建立購物車fragment中

--- app/src/main/java/com/ed/shunel/ShoppingcartFragment.java
+++ app/src/main/java/com/ed/shunel/ShoppingcartFragment.java
@@ -1,12 +1,21 @@
 package com.ed.shunel;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ed.shunel.bean.Shopping_Cart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,15 +23,96 @@ import android.view.ViewGroup;
  */
 public class ShoppingcartFragment extends Fragment {
 
+    private static final String TAG="TAG_ShoppingcartFragment";
+    private Activity activity;
+    private RecyclerView rv_Shopping_Cart;
+    private List<Shopping_Cart> shopping_cartList;
     public ShoppingcartFragment() {
         // Required empty public constructor
     }
 
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity=getActivity();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shoppingcart, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        findViews(view);
+        /* 初始化資料,包含從其他Activity傳來的Bundle資料 ,Preference資枓 */
+        initData();
+        /* 設置必要的系統服務元件如: Services、BroadcastReceiver */
+        setSystemServices();
+        /* 設置View元件對應的linstener事件,讓UI可以與用戶產生互動 */
+        setLinstener();
+
+    }
+
+    private void setSystemServices() {
+    }
+
+    private void setLinstener() {
+
+        rv_Shopping_Cart.setAdapter(new shopp_cart_adprer(activity,shopping_cartList));
+
+
+    }
+
+    private void initData() {
+
+        shopping_cartList=getDate();
+
+    }
+
+    private List<Shopping_Cart> getDate() {
+
+        List<Shopping_Cart> shoppingCarts = new ArrayList<>();
+        shoppingCarts.add(new Shopping_Cart(01,01,1));
+        shoppingCarts.add(new Shopping_Cart(02,04,1));
+
+
+        return shoppingCarts;
+    }
+
+    private void findViews(View view) {
+
+        rv_Shopping_Cart=view.findViewById(R.id.rv_Shopping_Cart);
+        rv_Shopping_Cart.setLayoutManager(new LinearLayoutManager(activity));
+
+
+
+    }
+
+    private class shopp_cart_adprer extends RecyclerView.Adapter {
+        public shopp_cart_adprer(Activity activity, List<Shopping_Cart> shopping_cartList) {
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
