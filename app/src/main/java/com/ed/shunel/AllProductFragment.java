3cdf5e6362771fcfcf9fa645c8880c91ab35789e
fgh940329 <fgh940329@gmail.com>
2020-07-07 19:07:05 +0800

    no message

--- app/src/main/java/com/ed/shunel/AllProductFragment.java
+++ app/src/main/java/com/ed/shunel/AllProductFragment.java
@@ -1,6 +1,7 @@
 package com.ed.shunel;
 
 import android.app.Activity;
+import android.graphics.Rect;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -66,8 +67,6 @@ public class AllProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-
-
         return inflater.inflate(R.layout.fragment_all_product, container, false);
     }
 
@@ -75,23 +74,24 @@ public class AllProductFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
         recyclerView.setAdapter(new ProductAdapter_Sam(getContext(), product));
+//        recyclerView.addItemDecoration(new SpacesItemDecoration(2));
 
-        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+      // swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity));
         product = getProduct();
         showBooks(product);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                //讀取的圈圈 動畫
-                swipeRefreshLayout.setRefreshing(true);
-                showBooks(product);
-                //直到讀取完 結束
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        });
+//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//                //讀取的圈圈 動畫
+//                swipeRefreshLayout.setRefreshing(true);
+//                showBooks(product);
+//                //直到讀取完 結束
+//                swipeRefreshLayout.setRefreshing(false);
+//            }
+//        });
 
 
        // recyclerView.addItemDecoration(new SpacesItemDecoration(0));
@@ -146,27 +146,27 @@ public class AllProductFragment extends Fragment {
 
 
 
-//    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
-//        private int space;
-//
-//        public SpacesItemDecoration(int space) {
-//            this.space = space;
-//        }
-//
-//        @Override
-//        public void getItemOffsets(Rect outRect, View view,
-//                                   RecyclerView parent, RecyclerView.State state) {
-//            outRect.left = space;
-//            outRect.right = space;
-//            outRect.bottom = space;
-//
-//            // Add top margin only for the first item to avoid double space between items
-//            if (parent.getChildLayoutPosition(view) == 0) {
-//                outRect.top = space;
-//            } else {
-//                outRect.top = 0;
-//            }
-//        }
-//    }
+    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+        private int space;
+
+        public SpacesItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view,
+                                   RecyclerView parent, RecyclerView.State state) {
+            outRect.left = space;
+            outRect.right = space;
+            outRect.bottom = space;
+
+            // Add top margin only for the first item to avoid double space between items
+            if (parent.getChildLayoutPosition(view) == 0) {
+                outRect.top = space;
+            } else {
+                outRect.top = 0;
+            }
+        }
+    }
 
 }
