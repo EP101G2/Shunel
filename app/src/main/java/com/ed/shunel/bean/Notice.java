050e77a0ad4e3e66cb3d874c4bc063deda257973
lin3253job <linlin3253job@gmail.com>
2020-07-11 15:53:36 +0800

    Notice_Fragment更改

--- app/src/main/java/com/ed/shunel/bean/Notice.java
+++ app/src/main/java/com/ed/shunel/bean/Notice.java
@@ -5,13 +5,17 @@ import java.sql.Timestamp;
 public class Notice {
     private int Notice_ID;
     private String Notice_Content;
+    private String Notice_Title;
     private Timestamp Notice_time;
     private int NOTICE_CATEGORY_ID;
     private int CATEGORY_MESSAGE_ID;
 
-    public Notice(int notice_ID, String notice_Content, Timestamp notice_time, int NOTICE_CATEGORY_ID, int CATEGORY_MESSAGE_ID) {
+
+
+    public Notice(int notice_ID, String notice_Content, String notice_Title, Timestamp notice_time, int NOTICE_CATEGORY_ID, int CATEGORY_MESSAGE_ID) {
         Notice_ID = notice_ID;
         Notice_Content = notice_Content;
+        Notice_Title = notice_Title;
         Notice_time = notice_time;
         this.NOTICE_CATEGORY_ID = NOTICE_CATEGORY_ID;
         this.CATEGORY_MESSAGE_ID = CATEGORY_MESSAGE_ID;
@@ -23,6 +27,14 @@ public class Notice {
 
     public void setNotice_ID(int notice_ID) {
         Notice_ID = notice_ID;
+    }
+
+    public String getNotice_Title() {
+        return Notice_Title;
+    }
+
+    public void setNotice_Title(String notice_Title) {
+        Notice_Title = notice_Title;
     }
 
     public String getNotice_Content() {
