a41bab2525ad7292a9f27e7ba8e3317b8d55131f
xuzixiang <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-07-13 14:51:24 +0800

    建立聊天室

---
+++ app/src/main/java/com/ed/shunel/Task/ChatWebSocketClient.java
@@ -0,0 +1,42 @@
+package com.ed.shunel.Task;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.util.Map;
+
+class ChatWebSocketClient extends WebSocketClient {
+    public ChatWebSocketClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    public ChatWebSocketClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public ChatWebSocketClient(URI serverUri, Draft draft, Map<String, String> headers, int connecttimeout) {
+        super(serverUri, draft, headers, connecttimeout);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+
+    }
+
+    @Override
+    public void onMessage(String message) {
+
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+
+    }
+
+    @Override
+    public void onError(Exception ex) {
+
+    }
+}
