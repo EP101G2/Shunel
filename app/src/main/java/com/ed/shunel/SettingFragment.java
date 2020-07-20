227cea7e030e2f6eee47f98529a8fdc129253d14
Yuan Che <edwarechang7878@hotmail.com>
2020-07-20 18:04:33 +0800

    no message

--- app/src/main/java/com/ed/shunel/SettingFragment.java
+++ app/src/main/java/com/ed/shunel/SettingFragment.java
@@ -1,10 +1,12 @@
 package com.ed.shunel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -12,11 +14,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.ed.shunel.Task.Common;
+
 
 public class SettingFragment extends Fragment {
 
-    Activity activity;
+     Activity activity;
     private LinearLayout btMInformation;
+    private Button btSignOut;
 
 
     @Override
@@ -38,13 +43,24 @@ public class SettingFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         btMInformation = view.findViewById(R.id.btMInformation);
-
+        btSignOut= view.findViewById(R.id.btSignOut);
         btMInformation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Navigation.findNavController(view).navigate(R.id.action_settingFragment_to_modifyFragment);
 
+            }
+        });
+
+        btSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(activity, MainActivity.class);   //前放目前ＡＣＴＩＶＩＴＹ，後放目標的ＡＣＴ
+                startActivity(intent);  //啟動跳頁動作\
+                Common.getPreherences(activity).edit().clear().apply();
+//                activity.finish();//把自己關掉
             }
         });
     }
