c585113dbe7b976a9e34fe2072101a6ad5f936d5
limitis0 <tw85lee@gmail.com>
2020-07-21 15:14:17 +0800

    no message

--- app/src/main/java/com/ed/shunel/OrderListFragment.java
+++ app/src/main/java/com/ed/shunel/OrderListFragment.java
@@ -27,6 +27,7 @@ import android.widget.TextView;
 import com.ed.shunel.Task.Common;
 import com.ed.shunel.Task.CommonTask;
 import com.ed.shunel.Task.ImageTask;
+import com.ed.shunel.bean.Order_Main;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
@@ -51,7 +52,7 @@ public class OrderListFragment extends Fragment{
     private ImageView ivNotYetDelivered, ivDelivered, ivReceived, ivCanceled, ivRefounded;
     private SwipeRefreshLayout swipeRefreshLayout;
     private ImageTask orderImageTask;
-    List<Orders> ordersList;
+    List<Order_Main> ordersList;
     RecyclerView rvOrderList;
     private CommonTask ordersListGetAllTask;
 
@@ -82,7 +83,7 @@ public class OrderListFragment extends Fragment{
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_order_list, container, false);
     }
-    //set up for rvOrderList
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -105,87 +106,102 @@ public class OrderListFragment extends Fragment{
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
-<<<<<<< HEAD
-
-//        imageViews, onClick Listener(select from status)
-        ivNotYetDelivered = ivNotYetDelivered.findViewById(R.id.ivNotYetDelivered);
-        ivDelivered = ivDelivered.findViewById(R.id.ivDelivered);
-        ivReceived = ivReceived.findViewById(R.id.ivReceived);
-        ivCanceled = ivCanceled.findViewById(R.id.ivCanceled);
-        ivRefounded = ivRefounded.findViewById(R.id.ivRefounded);
-=======
-//        searchViews, onClick Listener
+//        imageViews, onClick Listener, statement filter
         ivNotYetDelivered = view.findViewById(R.id.ivNotYetDelivered);
         ivDelivered = view.findViewById(R.id.ivDelivered);
         ivReceived = view.findViewById(R.id.ivReceived);
         ivCanceled = view.findViewById(R.id.ivCanceled);
         ivRefounded = view.findViewById(R.id.ivRefounded);
->>>>>>> 7479e0d5029386defce70bfdf16a1acab0a92e93
 
-
-
-//        svNotYetDelivered.setOnSearchClickListener(new SearchView.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
-//                adapter.getFilter().filter("0"); //?? setting the key: orderstatus.case0
-//            }
-//        });
-//        svDelivered.setOnSearchClickListener(new SearchView.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
-//                adapter.getFilter().filter("1"); //?? setting the key: orderstatus.case1
-//            }
-//        });
-//        svReceived.setOnSearchClickListener(new SearchView.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
-//                adapter.getFilter().filter("2"); //?? setting the key: orderstatus.case2
-//            }
-//        });
-//        svCanceled.setOnSearchClickListener(new SearchView.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
-//                adapter.getFilter().filter("3"); //?? setting the key: orderstatus.case3
-//            }
-//        });
-//        svRefounded.setOnSearchClickListener(new SearchView.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
-//                adapter.getFilter().filter("4"); //?? setting the key: orderstatus.case4
-//            }
-//        });
+        ivNotYetDelivered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
+                adapter.getFilter().filter("0"); //?? setting the key: orderstatus.case0
+            }
+        });
+        ivDelivered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
+                adapter.getFilter().filter("1"); //?? setting the key: orderstatus.case1
+            }
+        });
+        ivReceived.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
+                adapter.getFilter().filter("2"); //?? setting the key: orderstatus.case2
+            }
+        });
+        ivCanceled.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
+                adapter.getFilter().filter("3"); //?? setting the key: orderstatus.case3
+            }
+        });
+        ivRefounded.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderListFragment.OrderListAdapter adapter = (OrderListFragment.OrderListAdapter)rvOrderList.getAdapter();
+                adapter.getFilter().filter("4"); //?? setting the key: orderstatus.case4
+            }
+        });
     }
 
+    private List<Order_Main> getOrders() {
+        List<Order_Main> ordersList = null;
+        if (Common.networkConnected(activity)) {
+            String url = Common.URL_SERVER + "Orders_Servlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "getAll");
+            String jsonOut = jsonObject.toString();
+            ordersListGetAllTask = new CommonTask(url, jsonOut);
+            try {
+                String jsonIn = ordersListGetAllTask.execute().get();
+                Type listType = new TypeToken<List<Order_Main>>() {
+                }.getType();
+                ordersList = new Gson().fromJson(jsonIn, listType);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        } else {
+            Common.showToast(activity, R.string.textNoNetwork);
+        }
+        return ordersList;
+    }
+
+    private void showOrders(List<Order_Main> ordersList) {
+        if (ordersList == null || ordersList.isEmpty()) {
+            Common.showToast(activity, R.string.textnofound);
+        }
+        OrderListAdapter orderListAdapter = (OrderListAdapter) rvOrderList.getAdapter();
+        // 如果spotAdapter不存在就建立新的，否則續用舊有的
+        if (orderListAdapter == null) {
+            rvOrderList.setAdapter(new OrderListAdapter(activity, ordersList));
+        } else {
+            orderListAdapter.setOrders(ordersList);
+            orderListAdapter.notifyDataSetChanged();
+        }
+    }
     //    adapter for orderList
     private class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.PageViewHolder> {//implements Filterable
         private LayoutInflater layoutInflater;
         private int imageSize;
         Context context;
-        List<Orders> ordersList;
-        List<Orders> sortedOrderList = new ArrayList<>();
-//        OrderListAdapter.StatusFilter statusFilter;//initialise a filter
+        List<Order_Main> ordersList;
+        List<Order_Main> sortedOrderList = new ArrayList<>();
+        StatusFilter statusFilter;//initialise a filter
 
-        public OrderListAdapter(Context context, List<Orders> ordersList) {
+        public OrderListAdapter(Context context, List<Order_Main> ordersList) {
             this.context = context;
             layoutInflater = LayoutInflater.from(context);
             this.ordersList = ordersList;
             imageSize = getResources().getDisplayMetrics().widthPixels / 4;
         }
-        //    set up filter
-//        @Override
-//        public Filter getFilter() { //create the method: getFilter; when no statusFilter found, create a new one
-//            if (statusFilter == null){
-//                statusFilter = new OrderListAdapter.StatusFilter();
-//            }
-//            return statusFilter;
-//        }
 
-        void setOrders(List<Orders> ordersList) {
+        void setOrders(List<Order_Main> ordersList) {
             this.ordersList = ordersList;
         }
 
@@ -216,18 +232,19 @@ public class OrderListFragment extends Fragment{
 
         @Override
         public void onBindViewHolder(@NonNull PageViewHolder holder, int position) {
-            final Orders orders = ordersList.get(position);
+            final Order_Main orders = ordersList.get(position);
 
             String url = Common.URL_SERVER + "Orders_Servlet";
-            int id = orders.getOrderId();
+            int id = orders.getOrder_ID();
             orderImageTask = new ImageTask(url, id, imageSize, holder.ivOrderProductPic);
             orderImageTask.execute();
-
-            holder.ivOrderProductPic.setImageResource(orders.getImageId());
+//            holder.ivOrderProductPic.setImageResource(orders.getImageId());
             holder.tvOrderIdText.setText(R.string.textOrderIdText);
-            holder.tvOrderId.setText(String.valueOf(orders.getOrderId()));
+            holder.tvOrderId.setText(String.valueOf(orders.getOrder_ID()));
             holder.tvOrderStatusText.setText(R.string.textOrderStatusText);
-            holder.tvOrderStatus.setText(String.valueOf(orders.getOrderStatus()));
+            holder.tvOrderStatus.setText(String.valueOf(orders.getOrder_Main_Order_Status()));
+
+
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -239,106 +256,77 @@ public class OrderListFragment extends Fragment{
             });
         }//ok
 
+        //    set up filter
+//        @Override
+        public Filter getFilter() { //create the method: getFilter; when no statusFilter found, create a new one
+            if (statusFilter == null){
+                statusFilter = new OrderListAdapter.StatusFilter();
+            }
+            return statusFilter;
+        }
 
+        public class StatusFilter extends Filter{ //the StatusFilter extends Filter, with two methods
+            //                        public void getOrderStatus(Order_Main orders){
+//                int orderStatus = orders.getOrderStatus();
+//            }
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+//            constraint: represent the "orderStatus" from method adapter.getFilter().filter(orderStatus)
+//            status filter is the condition word of the filter
+//            the method "performFiltering()" is to do the filtering of the data(orderList)
+            public FilterResults performFiltering(CharSequence constraint) {
+//                filter by orderStatus. if orderStatus = 0
+                List<Order_Main> sortedOrdersList = new ArrayList<>();
+                if (constraint == null){
+                    sortedOrdersList.addAll(ordersList);
+                }else {
+                    for (Order_Main orders : ordersList){
+                        if (Objects.equals(orders.getOrder_Main_Order_Status(), constraint)){//Objects.equals(orders.getOrderStatus(), constraint)
+                            sortedOrdersList.add(orders);
+                        }
+                    }
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.count = ordersList.size();
+                filterResults.values = ordersList;
+                return filterResults;
+//                    switch (chosenOrderStatus) { //not yet completed!!
+//                        case R.id.svNotYetDelivered://click on 1st sv and shows only orderList.getOrderStatus(0) //write a method() but use dif parameter to adjust layout(search view ex)
+//                            chosenOrderStatus = 0;
+//
+//                            break;
+//                        case R.id.svDelivered://click on 2nd sv and shows only orderList.getOrderStatus(1)
+//
+//                            break;
+//                        case R.id.svReceived://click on 3rd sv and shows only orderList.getOrderStatus(2)
+//
+//                            break;
+//                        case R.id.svCanceled://click on 4th sv and shows only orderList.getOrderStatus(3)
+//
+//                            break;
+//                        case R.id.svRefounded://click on 5th sv and shows only orderList.getOrderStatus(4)
+//
+//                            break;
+//                        default:
+//                            sortedOrdersList.addAll(ordersList);
+//                    }
 
-//        public class StatusFilter extends Filter{ //the StatusFilter extends Filter, with two methods
-//            //                        public void getOrderStatus(Orders orders){
-////                int orderStatus = orders.getOrderStatus();
-////            }
-//            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-//            @Override
-////            constraint: represent the "orderStatus" from method adapter.getFilter().filter(orderStatus)
-////            status filter is the condition word of the filter
-////            the method "performFiltering()" is to do the filtering of the data(orderList)
-//            public FilterResults performFiltering(CharSequence constraint) {
-////                filter by orderStatus. if orderStatus = 0
-//                List<Orders> sortedOrdersList = new ArrayList<>();
-//                if (constraint == null){
-//                    sortedOrdersList.addAll(ordersList);
-//                }else {
+//                public showByStatus(){
 //                    for (Orders orders : ordersList){
-//                        if (Objects.equals(orders.getOrderStatus(), constraint)){//Objects.equals(orders.getOrderStatus(), constraint)
-//                            sortedOrdersList.add(orders);
+//                        if (orders.getOrderStatus().equals(chosenOrderStatus)){
+//                            sortedOrderList.add(orders);
 //                        }
 //                    }
-//                }
-////                    switch (chosenOrderStatus) { //not yet completed!!
-////                        case R.id.svNotYetDelivered://click on 1st sv and shows only orderList.getOrderStatus(0) //write a method() but use dif parameter to adjust layout(search view ex)
-////                            chosenOrderStatus = 0;
-////
-////                            break;
-////                        case R.id.svDelivered://click on 2nd sv and shows only orderList.getOrderStatus(1)
-////
-////                            break;
-////                        case R.id.svReceived://click on 3rd sv and shows only orderList.getOrderStatus(2)
-////
-////                            break;
-////                        case R.id.svCanceled://click on 4th sv and shows only orderList.getOrderStatus(3)
-////
-////                            break;
-////                        case R.id.svRefounded://click on 5th sv and shows only orderList.getOrderStatus(4)
-////
-////                            break;
-////                        default:
-////                            sortedOrdersList.addAll(ordersList);
-////                    }
-//
-////                public showByStatus(){
-////                    for (Orders orders : ordersList){
-////                        if (orders.getOrderStatus().equals(chosenOrderStatus)){
-////                            sortedOrderList.add(orders);
-////                        }
-////                    }
-////                    return sortedOrdersList;
-////                }//dumped
-//
-//                FilterResults filterResults = new FilterResults();
-//                filterResults.count = ordersList.size();
-//                filterResults.values = ordersList;
-//                return filterResults;
-//            }
-//            @Override
-//            public void publishResults(CharSequence constraint, FilterResults filterResults) {
-//                sortedOrderList.clear();
-//                sortedOrderList.addAll((List)filterResults.values);
-//                notifyDataSetChanged();
-//            }
-//        }//probably ok(?
-    }
+//                    return sortedOrdersList;
+//                }//dumped
 
-    private List<Orders> getOrders() {
-        List<Orders> ordersList = null;
-        if (Common.networkConnected(activity)) {
-            String url = Common.URL_SERVER + "Orders_Servlet";
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("action", "getAll");
-            String jsonOut = jsonObject.toString();
-            ordersListGetAllTask = new CommonTask(url, jsonOut);
-            try {
-                String jsonIn = ordersListGetAllTask.execute().get();
-                Type listType = new TypeToken<List<Orders>>() {
-                }.getType();
-                ordersList = new Gson().fromJson(jsonIn, listType);
-            } catch (Exception e) {
-                Log.e(TAG, e.toString());
             }
-        } else {
-            Common.showToast(activity, R.string.textNoNetwork);
-        }
-        return ordersList;
-    }
-
-    private void showOrders(List<Orders> ordersList) {
-        if (ordersList == null || ordersList.isEmpty()) {
-            Common.showToast(activity, R.string.textnofound);
-        }
-        OrderListAdapter orderListAdapter = (OrderListAdapter) rvOrderList.getAdapter();
-        // 如果spotAdapter不存在就建立新的，否則續用舊有的
-        if (orderListAdapter == null) {
-            rvOrderList.setAdapter(new OrderListAdapter(activity, ordersList));
-        } else {
-            orderListAdapter.setOrders(ordersList);
-            orderListAdapter.notifyDataSetChanged();
-        }
+            @Override
+            public void publishResults(CharSequence constraint, FilterResults filterResults) {
+                sortedOrderList.clear();
+                sortedOrderList.addAll((List)filterResults.values);
+                notifyDataSetChanged();
+            }
+        }//probably ok(?
     }
 }
