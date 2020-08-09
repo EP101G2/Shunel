1f0911a80a8f0cebb772c9e6ac1db00941d15dde
Yuan Che <edwarechang7878@hotmail.com>
2020-08-09 15:21:47 +0800

    no message

--- app/src/main/java/com/ed/shunel/HomeFragment.java
+++ app/src/main/java/com/ed/shunel/HomeFragment.java
@@ -64,7 +64,7 @@ public class HomeFragment extends Fragment {
         }
 
 //        if (getToken ==)
-        Log.e("11111", getToken);
+//        Log.e("11111", getToken);
 
 
         searchView = view.findViewById(R.id.rearchitem);
